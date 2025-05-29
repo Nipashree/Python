3e79e749a21778ce9426346cba808439bd90a77c
Nipashree <146835960+Nipashree@users.noreply.github.com>
2025-05-29 13:15:57 +0530

    Activity

---
+++ Greatest of 3 nos.py
@@ -0,0 +1,11 @@
+a=float(input("NUMBER1:"))
+b=float(input("NUMBER2:"))
+c=float(input("NUMBER3:"))
+if(a>b and a>c):
+    print(a)
+elif(a<b and b>c):    
+    print(b)
+else:
+    print(c)
+
+        
